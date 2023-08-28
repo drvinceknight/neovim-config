63c85528e551dae0cfa595c6dd8f8376ba57527e
Vince Knight <vince@vknight.org>
2023-08-28 11:25:24 +0100

    Add cmake lsp

--- lua/vince/plugins/lsp/mason.lua
+++ lua/vince/plugins/lsp/mason.lua
@@ -27,6 +27,7 @@ mason_lspconfig.setup({
 		"marksman",
 		"lua_ls",
 		"ltex",
+		"cmake",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
